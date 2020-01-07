66b6f6f3007916a93b1a18c21b7034ceb0790d74
Hieu Nguyen <16521675@gm.uit.edu.vn>
2020-01-08 01:42:20 +0700

    complete

--- presentation/productP.php
+++ presentation/productP.php
@@ -269,9 +269,47 @@ class ProductP
 			$total = $total + $value;
 		}
 		$product = <<<DELIMITER
-			<h3 class="total-price">
-			{$total}$
+			<h3 class="total-price mt-3">
+			Total: {$total}$
 			</h3>
+			<div class="marginleft-paypal">
+			<div class="paypal-container" id="paypal-button-container"></div>
+			</div>
+			<!-- Set up a container element for the button -->
+					
+			<!-- Include the PayPal JavaScript SDK -->
+			<script src="https://www.paypal.com/sdk/js?client-id=AVYELqUnYbYWQiTgw8K6o_g2wTL6MyyxYj9afnp6Iy-22WL1Akc7z_za3utm_GH4OqXlf2APFOZ3Ldzi&currency=USD"></script>
+			<script>
+				// Render the PayPal button into #paypal-button-container
+				paypal.Buttons({
+					locale: 'en_US',
+					style: {
+						label:'pay',
+						color: 'blue',
+						shape: 'pill',
+						size: 'small',
+						layout: 'horizontal',
+						tagline:false
+					},
+								// Set up the transaction
+					createOrder: function(data, actions) {
+						return actions.order.create({
+							purchase_units: [{
+								amount: {
+									value: "{$total}" //Price
+								}
+							}]
+						});
+					},
+					// Finalize the transaction
+					onApprove: function(data, actions) {
+						return actions.order.capture().then(function(details) {
+							// Show a success message to the buyer
+							alert('Transaction completed by ' + details.payer.name.given_name + '!');
+						});
+					}
+				}).render('#paypal-button-container');
+			</script>
 			DELIMITER;
 		echo $product;
 	}
